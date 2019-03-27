ac3d781a1bc92985adbed0511c648c1cd5ef219e
dark blue <29714452+dano5190@users.noreply.github.com>
2019-03-27 13:46:12 -0700

    tested the hikingtrails api with the geocoding npm and it works so far, will modify in the future

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Geocode from "react-geocode";
+import axios from "axios";
 
 Geocode.setApiKey("AIzaSyAn5O0hUEyAV1CHW5-x0nNorupO5Mr-_JM");
 
@@ -14,6 +15,10 @@ class App extends Component {
         res => {
             const { lat, lng } = res.results[0].geometry.location;
             console.log(lat, lng);
+            axios.get("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon="+ lng + "&maxDistance=50&maxResults=100&sort=distance&key=200431883-69df4d2d08177641033f4b4a848ab12d")
+            .then((response => 
+              console.log(response.data.trails)
+            ));
         },
         error => {
             console.error(error);
